d9d888500b2a402ea9cb3ece5ee8b666c8fa01da
aymazafar <148557450+aymazafar@users.noreply.github.com>
2025-03-21 10:44:23 +0500

    bugin code

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ def add_numbers(a, b):
     return a + b
 
 def test_addition():
-    assert add_numbers(10, 15) == 25
+    assert add_numbers(10, 15) == 30
     assert add_numbers(-5, 5) == 0
     assert add_numbers(0, 0) == 0
     assert add_numbers(3.5, 2.5) == 6.0
